c86bce1f5adefe2e15228424c34c7d03063cc521
Calel Costa <calel.costa@gmail.com>
2025-02-06 15:16:46 -0300

    :tada: Add JWT Configuration and Create Models and Records

---
+++ src/main/java/recargapay/wallet/DesafiorecargapayApplication.java
@@ -0,0 +1,13 @@
+package recargapay.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiorecargapayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafiorecargapayApplication.class, args);
+    }
+
+}
